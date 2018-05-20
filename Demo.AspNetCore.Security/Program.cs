402c84cf30dd606e328e463309adcf9b1691d8e0
Tomasz Pęczek <tpeczek@gmail.com>
2018-05-20 19:55:46 +0200

    Upgrade to ASP.NET Core 2.1.0 RC1

--- Demo.AspNetCore.Security/Program.cs
+++ Demo.AspNetCore.Security/Program.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using System.IO;
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
 
 namespace Demo.AspNetCore.Security
 {
@@ -7,14 +7,13 @@ namespace Demo.AspNetCore.Security
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
-
-            host.Run();
+            CreateWebHostBuilder(args)
+                .Build()
+                .Run();
         }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .UseStartup<Startup>();
     }
 }
--- Demo.AspNetCore.Security/Startup.cs
+++ Demo.AspNetCore.Security/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.DependencyInjection;
 using Lib.AspNetCore.Security;
@@ -12,7 +13,7 @@ namespace Demo.AspNetCore.Security
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddTransient<ISecurityHeadersReportingService, LoggerSecurityHeadersReportingService>();
-            services.AddMvc();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
